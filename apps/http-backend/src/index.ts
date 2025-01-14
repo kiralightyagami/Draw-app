0b7d0c2717ad0edbfd7054da2fc19bd1a8bbb8bc
kiralightyagami <crucisgamer98@gmail.com>
2025-01-14 10:17:34 +0530

    init

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,54 @@
+import  express  from "express";
+import  jwt  from "jsonwebtoken";
+import { middleware } from "./middleware";
+import {JWT_SECRET} from "@repo/backend-common/config";
+import {CreateUserSchema , SigninSchema , CreateRoomSchema} from "@repo/common/types";
+const app = express();
+
+app.post("/signup", ( req, res) => {
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    res.json({
+        userId: "123"
+    })
+})
+
+app.post("/signin", (req,res)=>{
+    
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET );
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room", middleware, (req,res)=>{
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    res.json({
+        roomId: 123
+    })
+})
+
+app.listen(3001);
