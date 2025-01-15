00b97afb5a533a90feb5533c3ae7e92de6eed862
kiralightyagami <crucisgamer98@gmail.com>
2025-01-15 21:43:57 +0530

    new endpoint in http-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -97,4 +97,21 @@ app.post("/room", middleware, async (req,res)=>{
     }
 })
 
+app.get("/chats/:roomId", async (req,res) =>{
+    const roomId = Number(req.params.roomId);
+    const messages = await prismaClient.chat.findMany({
+        where: {
+            roomId: roomId
+        },
+        orderBy: {
+            id: "desc"
+        },
+        take: 50
+    });
+
+    res.json({
+        messages: "success"
+    })
+})
+
 app.listen(3001);
